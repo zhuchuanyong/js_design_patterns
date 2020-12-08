b7596a79f514e199f77e293e714cb2a141159874
zhuchuanyong <zhu1251637837@gmail.com>
2020-12-08 14:12:23 +0800

    feat: 单例

--- src/02单例模式/02singleton.ts
+++ src/02单例模式/02singleton.ts
@@ -3,26 +3,35 @@
  * 一个类只有一个实例,且只会实例化一次
  */
 
+//  应用场景
+// 连接数据库，防止多次连接和断开
+// 购物车场景
+// 用户登录
+// vuex
+// 全局 loading 遮罩层
+// 生产唯一序列号
+
 class Singleton {
-    private static instance: Singleton;
-    /**
-     * 静态方法
-     */
-    private constructor() {}
-    public static getInstance() {
-        return function() {
-            if (!Singleton.instance) {
-                Singleton.instance = new Singleton();
-            }
-            return Singleton.instance;
-        };
-    }
-    login() {
-        console.log('login');
-    }
+  private static instance: Singleton;
+  /**
+   * 静态方法
+   */
+  private constructor() {}
+  public static getInstance() {
+    return function () {
+      if (!Singleton.instance) {
+        Singleton.instance = new Singleton();
+      }
+      return Singleton.instance;
+    };
+  }
+  login() {
+    console.log("login");
+  }
 }
 let ob1 = Singleton.getInstance()();
 console.log(ob1);
 let ob2 = Singleton.getInstance()();
 ob1.login();
 console.log(ob1 === ob2);
+
